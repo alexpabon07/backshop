d43568968510c82723b78b253fcad87cbc948a33
Alexander Pabon <alex312-@hotmail.com>
2021-09-11 22:55:03 -0500

    Base structure of the project

---
+++ src/main/java/com/alexpabon07/backshop/BackshopApplication.java
@@ -0,0 +1,13 @@
+package com.alexpabon07.backshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexpabon07/backshop/BackshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexpabon07.backshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
